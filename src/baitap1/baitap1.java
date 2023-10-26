7817c4f79a784191a0b8eae40ca5d29de64127e8
Nhatphuong11 <phuong201101@gmail.com>
2023-10-26 18:55:20 +0700

    first commit

---
+++ src/baitap1/baitap1.java
@@ -0,0 +1,21 @@
+package baitap1;
+import java.util.Scanner;
+public class baitap1 {
+    public static void main(String[] args) {
+        sumOfTwo();
+    }
+
+    private static void sumOfTwo() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Nhập vào số thứ nhất: ");
+            int firstNumber = Integer.parseInt(sc.nextLine());
+            System.out.print("Nhập vào số thứ hai: ");
+            int secondNumber = Integer.parseInt(sc.nextLine());
+            System.out.print("Tổng của hai số " + firstNumber + " và " +  secondNumber + " là: " + (firstNumber + secondNumber));
+        } catch (NumberFormatException e) {
+            System.out.println("Bạn phải nhập vào số");
+            sumOfTwo();
+        }
+    }
+}
